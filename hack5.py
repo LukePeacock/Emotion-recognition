3b0ed60358ad8c9509d770c9b8aeadc6669b8cfe
Luke Peacock <lukepeacock13@gmail.com>
2019-11-02 22:47:16 +0000

    moved password creation into a function

--- hack5.py
+++ hack5.py
@@ -1,3 +1,25 @@
-from real_time_video import start_stream
+import hashlib
+#from real_time_video import start_stream
 
-print(start_stream(3,50))
+#emos = (start_stream(3,50))
+
+
+## Encryption function using SHA-2 256 bit hashing
+def encrypt_string(hash_string):
+    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+    
+## Turn emotions array into password by hashing each individually
+## then concat them into a string and hash that then return
+def create_password(emotions):
+    password = ""
+
+    for item in emos:
+        password+= encrypt_string(item)
+    newpass = encrypt_string(password)
+    return newpass
+
+# Test emotion array
+emos = ['happy', 'sad', 'happy']
+password = create_password(emos)
+print(password)
