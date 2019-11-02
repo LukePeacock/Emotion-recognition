32d58a564da18bb6559c3ba554ba60cbe8f8ee91
Luke Peacock <lukepeacock13@gmail.com>
2019-11-02 23:29:51 +0000

    updated readme

--- hack5.py
+++ hack5.py
@@ -11,15 +11,17 @@ def encrypt_string(hash_string):
     
 ## Turn emotions array into password by hashing each individually
 ## then concat them into a string and hash that then return
-def create_password(emotions):
+def create_password(emotions, emo_num, emo_dur):
     password = ""
 
     for item in emos:
         password+= encrypt_string(item)
+    password += encrypt_string(emo_num)
+    password += encrypt_string(emo_dur)
     newpass = encrypt_string(password)
     return newpass
 
 # Test emotion array
 emos = ['happy', 'sad', 'happy']
-password = create_password(emos)
-print(password)
+password = create_password(emos, emo_num, emo_dur)
+print(password, 3, 1)
