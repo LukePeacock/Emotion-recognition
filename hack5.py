b24fa80765b9de4c0953f868645febb19dbb6fd4
Luke Peacock <lukepeacock13@gmail.com>
2019-11-03 00:24:32 +0000

    added command line options to hack5

--- hack5.py
+++ hack5.py
@@ -1,8 +1,75 @@
-import hashlib
+"""\
+------------------------------------------------------------
+USE: python <PROGNAME> (options)
+OPTIONS:
+    -h : print this help message
+    -e : number of emotions to detect
+    -t : time per emotion
+------------------------------------------------------------\
+"""
+
+#===============================================
+from __future__ import print_function
+import sys, getopt, hashlib
+
 #from real_time_video import start_stream
 
-#emos = (start_stream(3,50))
+#========================================
+# Command Line Processing
+class CommandLine:
+    def __init__(self):
+        opts, args = getopt.getopt(sys.argv[1:], 'he:t:')
+        opts = dict(opts)
+        self.exit = True
 
+        if '-h' in opts:
+            self.printHelp()
+            return
+  
+        if len(args) > 0:
+            print("*** ERROR: no arg files - only options! ***", file=sys.stderr)
+            self.printHelp()
+            return
+        
+        ## check if emotion number option is included
+        if '-e' in opts:
+            if isinstance(int(opts['-e']), int):
+                self.emo_number = opts['-e']
+            else:
+                warning = (
+                    "*** ERROR: emotion number (opt: -e LABEL)! ***\n"
+                    " -- value (%s) not recognised!\n"
+                    " -- must be an integer"
+                    ) % (opts['-e'])
+                print(warning, file=sys.stderr)
+                self.printHelp()
+                return
+        else:
+            self.emo_number = 5
+
+        ## check if emotion duration option is included
+        if '-t' in opts:
+            if isinstance(float(opts['-t']), float):
+                self.emo_duration = opts['-t']
+            else:
+                warning = (
+                    "*** ERROR: emotion duration (opt: -t LABEL!) ***\n"
+                    " -- value (%s) not recognised!\n"
+                    " -- must be a float"
+                    ) % (opts['-t'])
+                print(warning, file=sys.stderr)
+                self.printHelp()
+                return
+        else:
+            self.emo_duration = 1
+    
+        self.exit = False
+    
+    def printHelp(self):
+        progname = sys.argv[0]
+        progname = progname.split('/')[-1]
+        help = __doc__.replace('<PROGNAME>', progname, 1)
+        print(help, file=sys.stderr)
 
 ## Encryption function using SHA-2 256 bit hashing
 def encrypt_string(hash_string):
@@ -21,9 +88,20 @@ def create_password(emotions, emo_num, emo_dur):
     newpass = encrypt_string(password)
     return newpass
 
-# Test emotion array
-emos = ['happy', 'sad', 'happy']
-emo_num = 3
-emo_dur = 1
-password = create_password(emos, str(emo_num), str(emo_dur))
-print(password)
+if __name__ == '__main__':
+    config = CommandLine()
+    if config.exit:
+        sys.exit(0)
+    emo_num = config.emo_number
+    emo_dur = config.emo_duration
+    print(str(emo_num) + ":" + str(emo_dur))
+    
+    
+    # Test emotion array
+    emos = ['happy', 'sad', 'happy']
+    password = create_password(emos, str(emo_num), str(emo_dur))
+    print(password)
+
+
+
+#emos = (start_stream(3,50))
